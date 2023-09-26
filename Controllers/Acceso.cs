7743b9de9717ed377d2a3ec76626862c9410d0ef
Drakeneess <srdrknss616@gmail.com>
2023-09-26 10:12:38 -0400

    Controller acceso

---
+++ Controllers/Acceso.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Final.Controllers
+{
+    public class Acceso : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
